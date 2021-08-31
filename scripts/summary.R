12f252c728a9bd403eb6cd34aad66ea84445830a
MaryemT <49489183+MaryemT@users.noreply.github.com>
2021-08-31 15:43:19 +0000

    Update summary_statistics : + plink check, + gi advice, +(correction read files delim)

--- scripts/summary.R
+++ scripts/summary.R
@@ -1,17 +1,24 @@
 #!/usr/bin/env Rscript
 args = commandArgs(trailingOnly = TRUE) 
 
-#   @MaryemT 
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---  
+#   Marieme Top
+#   @ : topmaryem@gmail.com
+#   IPD/ ECRDS/ BHI: avalanche-org: Gen_Assoc Project (H3ABioNet)
 
-#   avalanche-org
+#         This is code to generate summary statistics of your data   
+#         Advice for Running Genotype Inference | Doing an empirical analysis or not (dev) 
 
-#   /Gen_Assoc
+# --- Requirements 
+# Input     : ped, map and phen files given as arguments
+#           : User must provide clean dataset without Mendelian errors
+#           : All files in working directory
+# Dependency: Plink
 
-#-------  Summary
-#-------  ped, map and phen files given as arguments
-#         Should a problem occur, contact : NdeyeMarieme.top@pasteur.sn
+# --- Points for improvement: Ctrl+f : /!\ 
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- 
 
-#-------  Packages
+# --- --- ---  Packages
 
 if(("optparse" %in% rownames(installed.packages())) == F){
   install.packages("optparse", dependencies=TRUE, repos="http://cran.r-project.org")
@@ -22,39 +29,49 @@ if(("stringr" %in% rownames(installed.packages())) == F){
 if(("descr" %in% rownames(installed.packages())) == F){
   install.packages("descr", dependencies=TRUE, repos="http://cran.r-project.org")
 } 
+if(("dplyr" %in% rownames(installed.packages())) == F){
+  install.packages("dplyr", dependencies=TRUE, repos="http://cran.r-project.org")
+} 
 
-#-------  Functions
+# --- --- ---  Functions
 
-# -- [count the number of family the individual is implicated in]
+# -- [count the total number of nuclear families]
 Numb_trios <- function(dataset){
+  parents= tibble()
   for (i in dataset$V2){
-
-    for (i in intersect(dataset$V2,dataset$V3)){      #si i est pere
-
-      numb_f_partners = nrow(unique(dataset[dataset$V3 == i, c(3,4)]))
-      numb_couple = numb_couple + numb_f_partners
-    }
-    return(numb_couple)
+    parents= rbind(parents,dataset[dataset$V2 == i, c(3,4)])
   }
+  parents = parents[-c(which(parents$V3 == 0)), ]  #remove founders
+  return(nrow(unique(parents)))
 }
 
-#-------  Collect arguments
+# -- [return all alleles for a marker in a dataset]
+get_alleles <- function(marker_position,dataset){   
+  total_allele=tibble()
+  for(ind in 1:nrow(dataset)){
+    marker = gsub(" ", "", dataset[ind,marker_position])      # get alleles
+    alleles=tibble()
+    allele1=substr(marker, start = 1, stop = 1)
+    allele2 = substr(marker, start = 2, stop = 2)
+    
+    if (isTRUE(allele1 != "0" | allele2 != "0")){             # exclude missing genotypes
+      alleles=rbind(allele1,allele2) #  (allele1 | allele 2)  
+    }
+    total_allele= rbind(total_allele,alleles)                 
+  }
+  return(total_allele$V1)
+}
+
+
+# --- --- ---  Libraries
 
 library(optparse)
 library(stringr)
 library(descr)
+suppressMessages(library(dplyr))
 
-path = getwd()
+# --- --- ---  Collect arguments
 
-dt <- Sys.time()
-
-cat("_____________________________\n\n")
-cat("Working directory:",path,"\n")
-cat("Run started at: ",as.character(dt),"\n")
-
-
-
-# Options
 option_list = list(
   make_option(c("--pedfile"), type="character", help="ped file", metavar="character"),
   make_option(c("--mapfile"), type="character", help="map file", metavar="character"),
@@ -74,49 +91,54 @@ if(is.null(opt$pedfile)) {cat("Option --pedfile is required. \n Execution stoppe
 if(is.null(opt$mapfile)) {cat("Option --mapfile is required. \n Execution stopped.")}
 if(is.null(opt$phenfile)) stop(cat("Option --phenfile is required. \n Execution stopped."))
 
+
+cat("\n\n----- Summary Statistics -----------\n")
+cat("____________________________________\n\n")
+cat(" __ Working directory:",getwd(),"\n")
+cat(" __ Run started at: ",as.character(Sys.time()),"\n")
+
 # --- Files
 
 ped = read.delim(paste0(path_to_file, opt$pedfile), header = F , stringsAsFactors = F)
 map = read.delim(paste0(path_to_file, opt$mapfile), header = F , stringsAsFactors = F)
+if(ncol(map)==1){map = read.delim(paste0(path_to_file, opt$mapfile), header = F , stringsAsFactors = F, sep = " ")}
 phen = read.delim(paste0(path_to_file, opt$phenfile), header = F , stringsAsFactors = F)
-
-#phen = read.table(paste0(path_to_file, opt$phenfile), header = T)
-
-# --- Variables
-numb_couple = 0
+if(ncol(phen)==1){phen = read.delim(paste0(path_to_file, opt$phenfile), header = F , stringsAsFactors = F, sep = " ")}
 
 # ---   SUMMARY STATISTICS  ---
-# --- --- --- --- --- --- --- - 
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- 
 
 cat("\nGenerating Summary Statistics..\n")
+
 cat("\n   ---  Data loaded \n")
-cat("-- ", opt$pedfile,"\n-- ", opt$mapfile,"\n-- ", opt$phenfile,"\n")
+cat("\t", opt$pedfile,"\n\t", opt$mapfile,"\n\t", opt$phenfile,"\n")
 
 cat("\n   ---  Data description \n")   
-cat("-- ","Families :", length(unique(ped$V1)),"\n")
-cat("-- ","Founders :", length(which(ped$V3 == 0)),"\n")
-cat("-- ","Nuclear Families :", Numb_trios(ped),"\n\n")
-cat("   -  Sex description \n")
-cat("-- ",nrow(ped), "individuals","\t",length(which(ped$V5== "1")), "males", length(which(ped$V5== "2")), "females \n\n")
-cat("   -  Markers :\n")
-cat("-- ",nrow(map)," markers \n\n")
-cat("   -  Missing values \n")
-cat("-- ","missing at \t: ", length(ped[ped == '0 0']), " / ",nrow(ped)* (ncol(ped)-6),"positions \n")
-cat("-- ","Percentage \t:  ", (length(ped[ped == '0 0'])/(nrow(ped)* (ncol(ped)-6))) * 100,"%\n\n")
+cat("\tFamilies :", length(unique(ped$V1)),"\n")
+cat("\tFounders :", length(which(ped$V3 == 0)),"\n")
+cat("\tNonfounders :", nrow(ped)-length(which(ped$V3 == 0)),"\n")
+cat("\tNuclear Families :", Numb_trios(ped),"\n\n")
 
-cat("   -  Phenotype description \n")
+cat("\tSex description \n")
+cat("\t",nrow(ped), "individuals","\t",length(which(ped$V5== "1")), "males", length(which(ped$V5== "2")), "females \n\n")
+cat("\tVariants\n")
+cat("\t",nrow(map)," variants \n")
+cat("\tPhenotype \n")
+cat("\tin" ,opt$phenfile,"\t:" ,(ncol(phen)-2)," phenotype(s) detected\n\n")
 
-cat("-- ","in" ,opt$phenfile,"\t:" ,(ncol(phen)-2)," phenotype(s) detected\n\n")
-
+#list_phenotypes
 phenotypes = colnames(phen)[3:ncol(phen)]
+i= 1; count = 0
 
-i=1; j = 1; count = 0
-
-for (i in 1:length(phenotypes)){
+for (i in 1:length(phenotypes)){  # parcourir les phenotypes
+  # Phenotype name
+  pheno = phenotypes[i]   
+  pheno_value = phen[1, pheno]    # get first value to test it 
   
-  if(isFALSE(phen[, phenotypes[i]][j]%%1==0) && isTRUE(phen[, phenotypes[i]][j] != 0)){
-    count = count + 1
-  }
+  # Detect if quantitative or categorial
+  if(isFALSE(pheno_value%%1==0) && isTRUE(pheno_value!=0)){count = count + 1}
+  
+  # Display
   cat("-- ","Phenotype ",phenotypes[i], " : ")
   if(count>=1){
     cat(" quantitative -------------\n\n")
@@ -124,23 +146,34 @@ for (i in 1:length(phenotypes)){
   }
   if (count==0){
     cat(" categorial   -------------\n\n")
-
     cat(" Levels:\t", sort(unique(phen[,phenotypes[i]])),"\n Counts:\t", table(phen[,phenotypes[i]]),"\n")
-
   }
   
   cat("\n")
   count = 0
 }
+suppressMessages(rm(phenotypes,i))
 
+cat("   -  Missing genotypes \n")
+cat("\tmissing values at \t: ", length(ped[ped == '0 0']), "positions /",nrow(ped)* (ncol(ped)-6),"total\n")
+cat("\tPercentage missing values\t:  ", (length(ped[ped == '0 0'])/(nrow(ped)* (ncol(ped)-6))) * 100,"%\n\n")
 
-# --- Check for Mendelian errors
+# --- Advice: Genotype Inference
+cat("\tMethod sensitive to missing data, you are strongly recommended to use the genotype inference tool. \n\n\n")
 
-# plink_ = "/home/g4bbm/tools/Plink/plink"
+# --- Check for Mendelian errors  --- --- --- --- --- --- --- --- --- --- --- 
+  
+plink_ = "/home/g4bbm/tools/Plink/plink"   #/!\ : option?
+cat("--- Check Mendelian errors")
+cmd = paste0("cp ",paste0(path_to_file, opt$pedfile)," check_mendel.ped ; cp ",paste0(path_to_file, opt$mapfile)," check_mendel.map")
+system(cmd)
+system(paste0(plink_ ," --file check_mendel --mendel --out sample_check > file.log"))
+cat("\n")
+system("grep 'errors detected' file.log")
+cat("--")
+system("grep genotyping file.log")
+system("rm *check* file.log")
 
-#cat("--- Check Mendelian errors")
-# cmd = paste0("cp ",paste0(path_to_file, opt$pedfile)," check_mendel.ped ; cp ",paste0(path_to_file, opt$mapfile)," check_mendel.map")
-# system(cmd)
-# 
-# system(paste0(plink_ ," --file check_mendel --mendel --out sample_check"))
-# system("rm *check*")        
+# --------------------------------
+cat("\n\n--- Choose options to start analysis.. \n\n")
+

fd1d5256483e5bf4f4ac1f2788fb423c183a58da
Jukoo <funscript@outlook.fr>
2022-02-16 15:50:33 +0000

    add webpack bundler

---
+++ apps/webpack.config.js
@@ -0,0 +1,37 @@
+/*
+ * webpack.config.js :  mtdt  Gen-Assoc  
+ * copyright (c) 2022 , jUmarB@protonmail.com  Umar <jukoo>  
+ */  
+__kernel_file__ :  {  core = require("./kernel") }  
+const  { resolve } =  core["@node_module"]["path"]  
+
+const  [  
+    source_app  =  "./assets/js/bridge.js",
+    bundle_dir  =  "./assets/js/dist" 
+] =  process.argv.slice(3)  
+
+const  bundle_output_filename =   () =>  {  
+    let   file_entry  = source_app.split("/")  
+    file_entry =  file_entry[file_entry.length - 1 ]  
+    return `build_${file_entry}` 
+} 
+    
+
+module.exports  = {
+
+    ["entry"] :   {  
+        app   :  source_app 
+    } , 
+
+    ["output"]:{
+        filename  : bundle_output_filename()  ,
+        path      : resolve(__dirname , bundle_dir) 
+    }, 
+    
+    ["devServer"]  : { 
+        static     : { directory : __dirname }  , 
+        compress   : true , 
+        port       : process.env?.['PORT']  || 4000  
+
+    }  
+}

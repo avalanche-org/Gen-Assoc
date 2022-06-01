5a2918c2d8380b9465122ae3d7945cefb469742d
Jukoo <funscript@outlook.fr>
2022-06-01 13:44:59 +0000

    tutorial   page  + interaction

--- apps/assets/js/tuto_sec.js
+++ apps/assets/js/tuto_sec.js
@@ -2,8 +2,21 @@
 //! responsable  js  for tutorial 
 //! copyright  (c)  2022,  Umar <jukoo> . [jUmarB@protonmail.com] (github/jukoo)   
 
-import  {  log  } from  "./ops.js"  
+import  {  log  , _  } from  "./ops.js"  
 
+const  MTDTOOLROOT="/main" 
+
+let link_references  =  [... _.querySelectorAll("a.ui")]
+["filter"](link =>   link.hasAttribute("alt"))
+["map"](link =>  {  
+
+    link.addEventListener("click" ,evt =>  { 
+        localStorage["cnav"]  = link.getAttribute("alt")
+        location.href = MTDTOOLROOT 
+
+    }) 
+
+}) 
 
 
 
